2586b679930a9b0145a33aa893b7eff9b086b382
echnfblasalle <162983404+echnfblasalle@users.noreply.github.com>
2024-03-18 11:56:20 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,9 @@ import pandas as pd
 import numpy as np
 voc=pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vTaUESVFbUnxC9q5-d-BiQXk6JD4r--bTZ3lNcryvfCeKqJq-tnz3VLhPwS_bZ5WXsRGYkOrtgkLlTU/pub?output=csv')
 st.dataframe(voc)
-l=voc.shape[o]
+l=voc.shape[0]
 i=np.random.choice(range(l))
-word_fr=voc['Définition'].values[i]
-word_chi=voc['Hanzi'].values[i]
-st.write(word_fr+"Hanzi"+word_chi)
+word_fr=voc['definition'].values[i]
+word_chi=voc['hanzi'].values[i]
+st.write(word_fr+"hanzi"+word_chi)
  
