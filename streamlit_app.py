b7121416afdbea7d9b39cd99d62a76dcab9d73b6
echnfblasalle <162983404+echnfblasalle@users.noreply.github.com>
2024-03-18 12:57:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ st.write("Traduis:"+word_fr)
 for i in range(4):
   st.button(voc["Hanzi"].values[indices[i]],on_clich=is_correct,args=[indices[i],j])
 def is_correct(i,j):
-  if i==j
+  if i==j:
   st_write("Bravo")
 else:
 st_write("Raté")
