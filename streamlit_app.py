d1cc186db2068fe2009e2ff7f35804633778ff2b
echnfblasalle <162983404+echnfblasalle@users.noreply.github.com>
2024-03-18 11:55:12 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ voc=pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vTaUESVFbUnxC9q
 st.dataframe(voc)
 l=voc.shape[o]
 i=np.random.choice(range(l))
-word_fr=voc['definition'].values[i]
+word_fr=voc['Définition'].values[i]
 word_chi=voc['Hanzi'].values[i]
 st.write(word_fr+"Hanzi"+word_chi)
  
