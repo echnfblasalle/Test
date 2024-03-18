43f312857b56d42058b9565afe12771a9c1cb4aa
echnfblasalle <162983404+echnfblasalle@users.noreply.github.com>
2024-03-18 13:03:22 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,11 +13,11 @@ indices=np.random.choice(l,size=4,replace=False)
 j=np.random.choice(indices)
 word_fr=voc["Définition"].values[j]
 st.write("Traduis:"+word_fr)
-for i in range(4):
-  st.button(voc["Hanzi"].values[indices[i]],on_click=is_correct,args=(indices[i],j))
 def is_correct(i,j):
    if i==j:
      st_write("Bravo")
    else:
     st_write("Raté")
-  
+for i in range(4):
+  st.button(voc["Hanzi"].values[indices[i]],on_click=is_correct,args=(indices[i],j))
+
