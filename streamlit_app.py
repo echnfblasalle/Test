e9596cf0a7925ab873e63b429b472cc184621e8d
echnfblasalle <162983404+echnfblasalle@users.noreply.github.com>
2024-03-18 12:31:39 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,5 @@ i=np.random.choice(range(l))
 word_fr=voc['Définition'].values[i]
 word_chi=voc['Hanzi'].values[i]
 word_pin=voc['Pinyin'].values[i]
-st.write(word_fr+" "+word_chi+" "+word_pin)
+st.write(word_fr+"|"+word_chi+"|"+word_pin)
 st.button("refresh")
-indices=np.random.choices(range(l),h=4,replace=False)
