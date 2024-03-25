9468ea197b51870b4cf25fee3ef1bb7e57497f12
echnfblasalle <162983404+echnfblasalle@users.noreply.github.com>
2024-03-25 12:01:31 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,14 +3,13 @@ import pandas as pd
 import numpy as np
 voc = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSeARXO3MT92XWpg2IwyQOQ8Wi2upeEkqJvNJz5i3bRqHdJIrTchGBBclVu-3Jd1ohYKM4IxecgV64I/pub?output=csv ')
 l = voc.shape[0]
-indices = np.random.choice(l, size=4, replace=False)
+if "indice" in st.session_state:
+  indices=st.session_state["indices"]
+else:
+  indices = np.random.choice(l, size=4, replace=False)
 j = np.random.choice(indices)
 word_fr=voc['Définition'].values[j]
 st.write('Traduis: '+word_fr)
-
-st.session_state["my_var"]=1
-if "my war" in st.stession_state
-del st.session_state["my_var"]
 
 def is_correct(i, j):
   if i==j:
