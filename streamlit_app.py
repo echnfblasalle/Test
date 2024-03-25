333222050b32b853195fdf05a67dc196f0fec43b
echnfblasalle <162983404+echnfblasalle@users.noreply.github.com>
2024-03-25 12:03:21 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 voc = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSeARXO3MT92XWpg2IwyQOQ8Wi2upeEkqJvNJz5i3bRqHdJIrTchGBBclVu-3Jd1ohYKM4IxecgV64I/pub?output=csv ')
 l = voc.shape[0]
-if "indice" in st.session_state:
+if "indices" in st.session_state:
   indices=st.session_state["indices"]
 else:
   indices = np.random.choice(l, size=4, replace=False)
@@ -28,5 +28,3 @@ with col2:
         st.button(voc["Hanzi"].values[indices[i]], on_click= is_correct, args=(indices[i],j))
 
 
-st.button("Secondary button")  #st.button default type is secondary
-st.button("Primary button", type="primary")
